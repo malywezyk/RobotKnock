d9215c6f9175245ea188e45e41f47fc2bf03edd0
malywezyk <147445005+malywezyk@users.noreply.github.com>
2025-08-22 14:44:52 +0200

    Delete Packages directory
    jestem debilem

--- Packages/networker.lua
+++
@@ -1,4 +0,0 @@
-local REQUIRED_MODULE = require(script.Parent._Index["leifstout_networker@0.3.0"]["networker"])
-export type Server = REQUIRED_MODULE.Server 
-export type Client = REQUIRED_MODULE.Client 
-return REQUIRED_MODULE
